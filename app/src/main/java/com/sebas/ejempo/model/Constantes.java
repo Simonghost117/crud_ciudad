cdebfd58093e14852276ec52d9f9ce2557b132fd
Nikolas020 <nitolas020@gmail.com>
2025-03-25 10:21:39 -0500

    tabla cuidador creada por alejandro

--- app/src/main/java/com/sebas/ejempo/model/Constantes.java
+++ app/src/main/java/com/sebas/ejempo/model/Constantes.java
@@ -10,5 +10,6 @@ public class Constantes  {
     public static  int NUM_VERSION = 1;
     public static String SENTENCIA_TABLA = "create table Ciudad(codigo integer, nombre text)";
     public static String SENTENCIA_TABLA2 = "create table Departamento(codigo integer, nombre text)";
+    public static String SENTENCIA_TABLA11 = "create table Cuidador(codigo integer, nombre text)";
 
 }
