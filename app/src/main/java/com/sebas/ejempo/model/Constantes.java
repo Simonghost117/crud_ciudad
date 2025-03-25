c1e7ae17b9129b88b74564fbef1a5954e1d04616
carl0s01 <crlsmrsalazar@gmail.com>
2025-03-25 09:35:07 -0500

    cambios hechos por German

--- app/src/main/java/com/sebas/ejempo/model/Constantes.java
+++ app/src/main/java/com/sebas/ejempo/model/Constantes.java
@@ -10,5 +10,6 @@ public class Constantes  {
     public static  int NUM_VERSION = 1;
     public static String SENTENCIA_TABLA = "create table Ciudad(codigo integer, nombre text)";
     public static String SENTENCIA_TABLA2 = "create table Departamento(codigo integer, nombre text)";
+    public static String SENTENCIA_TABLA5 = "create table Empresas(codigo interger, nombre text");
 
 }
